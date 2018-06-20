dea64ef514a5dc53222fd2b24e49406ff4ee1e8f
hirokishinoda <rss23843@nifty.com>
2018-06-20 14:21:56 +0900

    [新規] 処理の大枠となるクラス.

---
+++ app/src/main/java/com/example/hirokishinoda/mazusearchgame/Task.java
@@ -0,0 +1,13 @@
+package com.example.hirokishinoda.mazusearchgame;
+
+import android.graphics.Canvas;
+
+public abstract class Task {
+
+        public boolean onUpdate(){
+            return true;
+        }
+
+        public void onDraw(Canvas c){
+        }
+}
