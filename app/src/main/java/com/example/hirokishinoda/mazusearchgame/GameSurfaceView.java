8f9c6eb85f13b4fab23314966f2178ea81efe24a
hirokishinoda <rss23843@nifty.com>
2018-06-27 20:16:47 +0900

    [変更] レイアウトに登録するSurfaceView.
    レイアウトに、このクラスを登録する形に変更.

--- app/src/main/java/com/example/hirokishinoda/mazusearchgame/GameSurfaceView.java
+++ app/src/main/java/com/example/hirokishinoda/mazusearchgame/GameSurfaceView.java
@@ -1,24 +1,26 @@
 package com.example.hirokishinoda.mazusearchgame;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class GameSurfaceView  implements SurfaceHolder.Callback,Runnable{
+public class GameSurfaceView  implements SurfaceHolder.Callback,Runnable,BottonTypesDefine{
     private GameManager gm = new GameManager();
-    private SurfaceView surfaceView;
     private Thread thread;
+    private SurfaceView sv;
 
-    public GameSurfaceView(Context context,SurfaceView sv){
-        surfaceView = sv;
-        surfaceView.getHolder().addCallback(this);
+    public GameSurfaceView(Context context,SurfaceView sv) {
+        this.sv = sv;
+        sv.getHolder().addCallback(this);
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
     }
 
     @Override
@@ -32,13 +34,30 @@ public class GameSurfaceView  implements SurfaceHolder.Callback,Runnable{
         thread = null;
     }
 
-    
     @Override
     public void run() {
         while(thread != null){
-            gm.onUpdate();
-            draw(surfaceView.getHolder());
+            gm.onUpdate(0,0);
+            draw(sv.getHolder());
         }
+    }
+
+    public void touchButton(int botton_type){
+        switch (botton_type){
+            case RIGHT_B:
+                gm.stage.move(1,0);
+                break;
+            case LEFT_B:
+                gm.stage.move(-1,0);
+                break;
+            case UP_B:
+                gm.stage.move(0,-1);
+                break;
+            case DOWN_B:
+                gm.stage.move(0,1);
+                break;
+        }
+
     }
 
     private void draw(SurfaceHolder holder){
