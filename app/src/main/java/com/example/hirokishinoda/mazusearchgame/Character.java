8f98c6609a0f43e9edcd91712ea564a07412cd72
hirokishinoda <rss23843@nifty.com>
2018-06-27 20:13:22 +0900

    [更新] キャラクターの描画に必要な定数を定義.

--- app/src/main/java/com/example/hirokishinoda/mazusearchgame/Character.java
+++ app/src/main/java/com/example/hirokishinoda/mazusearchgame/Character.java
@@ -1,5 +1,13 @@
 package com.example.hirokishinoda.mazusearchgame;
 
-public abstract class Character {
-    int character_x,character_y;
+public abstract class Character extends Task{
+    static final int RECTSIZE = 25;
+    static final double RADIUS = 12.5;
+    static final int OFFSETX = 25;
+    static final int OFFSETY = 110;
+
+    static int character_x,character_y;
+
+    public void move(int x,int y){
+    }
 }
