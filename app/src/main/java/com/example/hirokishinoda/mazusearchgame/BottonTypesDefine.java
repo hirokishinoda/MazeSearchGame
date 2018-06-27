a8e9e6c725b1f431d626d233f8748524e7270d96
hirokishinoda <rss23843@nifty.com>
2018-06-27 20:12:14 +0900

    [新規] ボタンの種類を定義するインターフェース.

--- app/src/main/java/com/example/hirokishinoda/mazusearchgame/BottonTypesDefine.java
+++ app/src/main/java/com/example/hirokishinoda/mazusearchgame/BottonTypesDefine.java
@@ -1,5 +1,10 @@
 package com.example.hirokishinoda.mazusearchgame;
 
 public interface BottonTypesDefine {
-    
+
+    public static final int RIGHT_B = 1;
+    public static final int LEFT_B = 2;
+    public static final int UP_B = 3;
+    public static final int DOWN_B = 4;
+
 }
