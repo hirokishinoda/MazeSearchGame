0872116d542f5cd94df5bc58d04ae96b1d99e157
hirokishinoda <rss23843@nifty.com>
2018-06-27 20:18:26 +0900

    [追加] SurfaceViewの作成、ボタンのアクションを追加.

--- app/src/main/java/com/example/hirokishinoda/mazusearchgame/MazeSearch.java
+++ app/src/main/java/com/example/hirokishinoda/mazusearchgame/MazeSearch.java
@@ -3,24 +3,56 @@ package com.example.hirokishinoda.mazusearchgame;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.SurfaceView;
+import android.view.View;
 import android.widget.Button;
 
-public class MazeSearch extends AppCompatActivity {
-    private GameSurfaceView gsv;
+public class MazeSearch extends AppCompatActivity implements BottonTypesDefine{
+    GameSurfaceView gsv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mazu_search);
 
-        SurfaceView sv = (SurfaceView)findViewById(R.id.SV);
+        SurfaceView sv = findViewById(R.id.SV);
         gsv = new GameSurfaceView(this,sv);
 
-        Button right_button = (Button)findViewById(R.id.button_right);
-
-        Button left_button = (Button)findViewById(R.id.button_left);
-        Button up_button = (Button)findViewById(R.id.button_up);
-        Button down_button = (Button)findViewById(R.id.button_down);
+        buttonEvents();
     }
-    
+
+    private void buttonEvents(){
+        Button right_b = findViewById(R.id.button_right);
+        Button left_b = findViewById(R.id.button_left);
+        Button up_b = findViewById(R.id.button_up);
+        Button down_b = findViewById(R.id.button_down);
+
+        right_b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gsv.touchButton(RIGHT_B);
+            }
+        });
+
+        left_b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gsv.touchButton(LEFT_B);
+            }
+        });
+
+        up_b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gsv.touchButton(UP_B);
+            }
+        });
+
+        down_b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gsv.touchButton(DOWN_B);
+            }
+        });
+    }
 }
+
