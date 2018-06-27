37b389b502a72d3c3191cbc2ddab559434c16f91
hirokishinoda <rss23843@nifty.com>
2018-06-27 20:19:22 +0900

    [追加] Playerの移動処理、描画処理を追加.

--- app/src/main/java/com/example/hirokishinoda/mazusearchgame/Player.java
+++ app/src/main/java/com/example/hirokishinoda/mazusearchgame/Player.java
@@ -1,4 +1,37 @@
 package com.example.hirokishinoda.mazusearchgame;
 
-public class Player {
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+public class Player extends Character{
+    MazeMap map;
+
+    public Player(){
+        super();
+        character_x = 0;
+        character_y = 0 ;
+    }
+
+    @Override
+    public void move(int x,int y) {
+                character_x += x;
+                character_y += y;
+    }
+
+    @Override
+    public void onDraw(Canvas c) {
+        super.onDraw(c);
+
+        Paint paint = new Paint();
+        paint.setColor(Color.GREEN);
+
+        paint.setAntiAlias(true);
+        c.drawCircle((float)(character_x * RECTSIZE + RADIUS + OFFSETX), (float)(character_y * RECTSIZE + RADIUS + OFFSETY), (float)RADIUS, paint);
+    }
+
+    public void setCharacterPoint(int x,int y){
+        character_x = x;
+        character_y = y;
+    }
 }
